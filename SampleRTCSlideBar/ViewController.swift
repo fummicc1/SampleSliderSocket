637b7aabf56927c8a1beddbae63d0dff0abc0a67
fummicc1 <fumiya989@gmail.com>
2019-02-21 15:00:19 +0900

    Initial Commit

---
+++ SampleRTCSlideBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleRTCSlideBar
+//
+//  Created by Fumiya Tanaka on 2019/02/21.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
